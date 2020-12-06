c2b4eb267d1136b17ab3d0dcd346627ca195d5b3
Phillip Seitzer <pmseitzer@ucdavis.edu>
2020-12-05 17:32:47 -0800

    Add code snippet from problem 4 text into code.

--- PS5_problem4.cpp
+++ PS5_problem4.cpp
@@ -2,7 +2,23 @@
 
 using namespace std;
 
+#define MAX_VALUE 1023
+void count_all(int input_array_size, int * input_array, int * output_array){
+    int counter;
+
+    for(counter = 0; counter < input_array_size; counter++) {
+        assert(input_array[counter] <= MAX_VALUE);
+        assert(input_array[counter] >= 0);
+        output_array[input_array[counter]]++;
+    }
+}
+
 int main(int argc, char *argv[])
 {
+    int input_array[MAX_VALUE];
+    int output_array[MAX_VALUE];
+
+    count_all(MAX_VALUE, input_array, output_array);
+
     cout << "All Processes Successfully Completed!" << endl;
 }
