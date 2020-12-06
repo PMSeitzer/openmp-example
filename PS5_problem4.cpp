157c148d5bd6c9289bc3e44feeece2e34ddb2baa
Phillip Seitzer <pmseitzer@ucdavis.edu>
2020-12-05 17:50:49 -0800

    Fix buffer errors.

--- PS5_problem4.cpp
+++ PS5_problem4.cpp
@@ -7,18 +7,25 @@ void count_all(int input_array_size, int * input_array, int * output_array){
     int counter;
 
     for(counter = 0; counter < input_array_size; counter++) {
+
         assert(input_array[counter] <= MAX_VALUE);
         assert(input_array[counter] >= 0);
+
         output_array[input_array[counter]]++;
     }
 }
 
 int main(int argc, char *argv[])
 {
-    int input_array[MAX_VALUE];
-    int output_array[MAX_VALUE];
+    int input_array[MAX_VALUE+1];
 
-    count_all(MAX_VALUE, input_array, output_array);
+    for (unsigned int i = 0 ; i < MAX_VALUE; i++) {
+        input_array[i] = i % 10;
+    }
+
+    int output_array[MAX_VALUE+1];
+
+    count_all(MAX_VALUE+1, input_array, output_array);
 
     cout << "All Processes Successfully Completed!" << endl;
 }
