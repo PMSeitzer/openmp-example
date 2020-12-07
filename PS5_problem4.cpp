03b0b2c10d791d3960c8e7595585e9e825172e8c
Phillip Seitzer <pmseitzer@ucdavis.edu>
2020-12-06 17:45:54 -0800

    Added mutexes to try to fix deadlocks.

--- PS5_problem4.cpp
+++ PS5_problem4.cpp
@@ -17,25 +17,63 @@ struct thread_data{
    int *input_array;
    int *output_array;
    int *output_lock_array;
+
+   pthread_cond_t  cv;
+   pthread_mutex_t mutex;
+   int     flag;
 };
 
 static struct thread_data thread_data_array[NUM_THREADS];
 
 void P(int* output_lock_array_ptr){
 
+    //lock this thread in preparation for exclusive access
+    pthread_mutex_lock(&thread_data_array[*output_lock_array_ptr].mutex);
+
+    //note that while loop is only entered if this part of the array is unavailable
+
     //check that this coordinate in the output array is not currently being accessed
-    while (*output_lock_array_ptr == 0) {
+    while (thread_data_array[*output_lock_array_ptr].flag == 0) {
+
+        /* When the current thread executes this
+         * pthread_cond_wait() statement, the
+         * current thread will be blocked on s->cv
+         * and (atomically) unlocks s->mutex !!!
+         * Unlocking s->mutex will let other thread
+         * in to test s->flag.... */
+        pthread_cond_wait(&(thread_data_array[*output_lock_array_ptr].cv),
+                &thread_data_array[*output_lock_array_ptr].mutex);
+
         //wait until the lock has been released by some other thread's V() call
-        cout << (*output_lock_array_ptr) << " waiting..." << endl;
+        //cout << (*output_lock_array_ptr) << " waiting..." << endl;
     }
 
-    //lock this thread in preparation for exclusive access
-    *output_lock_array_ptr = 0;
+    /* This will cause all other threads
+     * that executes a P() call to wait
+     * in the (above) while-loop !!! */
+    thread_data_array[*output_lock_array_ptr].flag = 0;
+
+    //release lock in preparation for access
+    pthread_mutex_unlock(&thread_data_array[*output_lock_array_ptr].mutex);
 }
 
 void V(int* output_lock_array_ptr) {
-    //release lock
-    *output_lock_array_ptr = 1;
+
+    //lock this thread in preparation for exclusive access
+    pthread_mutex_lock(&thread_data_array[*output_lock_array_ptr].mutex);
+
+    /* This call may restart some thread that
+     * was blocked on s->cv (in the P() call)
+     * if there was not thread blocked on
+     * cv, this operation does absolutely
+     * nothing...                          */
+    pthread_cond_signal(&thread_data_array[*output_lock_array_ptr].cv);
+
+    /* Update semaphore state to Up */
+    thread_data_array[*output_lock_array_ptr].flag = 1;
+
+    //release lock in preparation for access
+    pthread_mutex_unlock(&thread_data_array[*output_lock_array_ptr].mutex);
 }
 
 void count_all(int input_array_size, int *input_array, int *output_array, int *output_lock_array){
@@ -51,6 +89,7 @@ void count_all(int input_array_size, int *input_array, int *output_array, int *o
 
         cout << "processing entry #" << counter << ", with value=" << input_array[counter] << endl;
 
+        //change flag state
         P(&output_lock_array[counter]);
 
         //thread-safe work
@@ -88,6 +127,8 @@ int main(int argc, char *argv[]){
         thread_data_array[counter].input_array = &input_array[INPUT_ARRAY_ELEMENTS / NUM_THREADS * counter];
         thread_data_array[counter].output_array = output_array;
         thread_data_array[counter].output_lock_array = output_lock_array;
+
+        pthread_mutex_unlock(&thread_data_array[counter].mutex);
 
         pthread_create(&threads[counter], nullptr, function_starter, static_cast<void*>(&(thread_data_array[counter])));
     }
